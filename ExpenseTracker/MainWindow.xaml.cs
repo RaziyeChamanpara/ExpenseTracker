024335a80087115db5035ddbf394d0c89ff42921
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-02-19 16:14:27 +1100

    display data in DataGrid

--- ExpenseTracker/MainWindow.xaml.cs
+++ ExpenseTracker/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace ExpenseTracker
 {
     /// <summary>
@@ -23,6 +25,30 @@ namespace ExpenseTracker
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ExpenseContext expenseContext = new ExpenseContext();
+                expenseContext.ExpenseTypes.Add(new ExpenseType() { Id = 3, Name = "utilities" });
+                expenseContext.ExpenseTypes.Add(new ExpenseType() { Id = 4, Name = "daily" });
+
+                expenseContext.SaveChanges();
+                MessageBox.Show("Added");
+
+                List<ExpenseType> expenseTypes= expenseContext.ExpenseTypes.ToList();
+                dataGrid.ItemsSource = expenseTypes;
+
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
         }
     }
 }
