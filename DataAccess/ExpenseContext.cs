47129088d1b1c5b437fcf7ad06421f64d16209e9
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-02-19 16:09:33 +1100

    add DataAccess project

---
+++ DataAccess/ExpenseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace DataAccess
+{
+    public class ExpenseContext:DbContext
+    {
+        public ExpenseContext()
+            :base("ExpenseTrackerConnectionString")
+        {
+
+        }
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+    }
+}
