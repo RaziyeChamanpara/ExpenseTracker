add0ce35df22254fd3980176c2fec7fb928cf3e9
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-02-28 17:10:49 +1100

    add ExpenseContext to DataAccess project

--- DataAccess/ExpenseContext.cs
+++ DataAccess/ExpenseContext.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Data.Entity;
 
 namespace DataAccess
 {
-    public class ExpenseContext:DbContext
+    public class ExpenseContext : DbContext
     {
-        public ExpenseContext()
-            :base("ExpenseTrackerConnectionString")
+        public ExpenseContext():
+            base("ExpenseTrackerConnectionString")
         {
 
         }
         public DbSet<ExpenseType> ExpenseTypes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
     }
 }
