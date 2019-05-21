3679736829e6ba65d2e663b6179a1cf02980e31e
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-05-21 19:32:10 +1000

    add ExpenseRepository to DataAccess

---
+++ DataAccess/ExpenseRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ExpenseRepository : IRepository<Expense>
+    {
+        private ExpenseContext _expenseContext = new ExpenseContext();
+
+        public void Add(Expense entity)
+        {
+            _expenseContext.Expenses.Add(entity);
+            _expenseContext.SaveChanges();
+        }
+
+        public void Remove(Expense entity)
+        {
+            _expenseContext.Expenses.Remove(entity);
+            _expenseContext.SaveChanges();
+        }
+
+        public Expense Get(int id)
+        {
+            return _expenseContext.Expenses
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+        }
+
+        public List<Expense> GetAll()
+        {
+            return _expenseContext.Expenses.ToList();
+        }
+
+    }
+}
---
+++ DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    interface IRepository<TEntity>
+    {
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Get(int id);
+        List<TEntity> GetAll();
+    }
+}
