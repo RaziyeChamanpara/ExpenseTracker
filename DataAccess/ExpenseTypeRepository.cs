f69d53dc7727b6181826bd40878ebd7bc7c16f68
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-06-19 14:35:11 +1000

    create ExpenseTypeRepository class

---
+++ DataAccess/ExpenseTypeRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ExpenseTypeRepository : IRepository<ExpenseType>
+    {
+
+        public void Add(ExpenseType expenseType)
+        {
+            using (ExpenseContext _expenseContext = new ExpenseContext())
+            {
+                _expenseContext.ExpenseTypes.Add(expenseType);
+                _expenseContext.SaveChanges();
+            }
+        }
+
+        public ExpenseType Get(int id)
+        {
+            using (ExpenseContext _expenseContext = new ExpenseContext())
+            {
+                return _expenseContext.ExpenseTypes.Where(x => x.Id == id).FirstOrDefault();
+            }
+        }
+
+        public List<ExpenseType> GetAll()
+        {
+            using (ExpenseContext _expenseContext = new ExpenseContext())
+            {
+                return _expenseContext.ExpenseTypes.ToList();
+            }
+        }
+
+        public void Remove(ExpenseType expenseType)
+        {
+            using (ExpenseContext _expenseContext = new ExpenseContext())
+            {
+                _expenseContext.ExpenseTypes.Remove(expenseType);
+            }
+        }
+
+        public void Update(ExpenseType entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
