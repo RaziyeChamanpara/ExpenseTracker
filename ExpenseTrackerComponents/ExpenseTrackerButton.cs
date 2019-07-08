48c51177ff271fe1348289dc5ae7f32b8e5f5cc3
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-07-08 15:00:23 +1000

    add ExpenseTrackerButton class

---
+++ ExpenseTrackerComponents/ExpenseTrackerButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ExpenseTrackerComponents
+{
+    public class ExpenseTrackerButton:Button
+    {
+        public ExpenseTrackerButton()
+        {
+            //Padding
+            this.Padding=new Thickness(10,3,10,3);
+
+            //Margin
+            Thickness margin=this.Margin;
+            margin.Left = 5;
+            margin.Right = 5;
+            margin.Top = 5;
+            this.Margin = margin;
+
+            //Foreground
+            this.Foreground = Brushes.White;
+
+        }
+    }
+}
