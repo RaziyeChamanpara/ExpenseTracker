5bcbeec55f4492ade7176ab32c773bdd982815a6
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-07-08 15:02:45 +1000

    create ExpenseTypesButton class

--- ExpenseTracker/ExpenseTypesWindow.xaml.cs
+++ ExpenseTracker/ExpenseTypesWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using DataAccess;
 
 namespace ExpenseTracker
@@ -28,6 +20,7 @@ namespace ExpenseTracker
         {
             InitializeComponent();
         }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             _expenseTypes = _expenseContext.ExpenseTypes.ToList();
---
+++ ExpenseTrackerComponents/ExpenseTypesButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace ExpenseTrackerComponents
+{
+    public class ExpenseTypesButton : ExpenseTrackerButton
+    {
+        public ExpenseTypesButton()
+        {
+            this.Background = Brushes.LightSalmon;
+            this.BorderBrush = Brushes.LightSalmon;
+        }
+    }
+}
