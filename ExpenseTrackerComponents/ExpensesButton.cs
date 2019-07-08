524e6a0f0435a807f1804eb52fb6cc6e01301807
Raziye Chamanpara <r.chamanpara@gmail.com>
2019-07-08 15:03:32 +1000

    create ExpensesButton class

---
+++ ExpenseTrackerComponents/ExpensesButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Media;
+namespace ExpenseTrackerComponents
+{
+    public class ExpensesButton:ExpenseTrackerButton
+    {
+        public ExpensesButton()
+        {
+            this.Background = Brushes.LightSteelBlue;
+            this.BorderBrush = Brushes.LightSteelBlue;
+        }
+    }
+}
