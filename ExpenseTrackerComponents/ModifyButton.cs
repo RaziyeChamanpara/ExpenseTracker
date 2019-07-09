4efd6e7c591ffed9365df3520bfb7eb3fc75c465
RaziyeChamanpara <r.chamanpara@gmail.com>
2019-07-09 13:51:26 +1000

    create ModifyButton class

---
+++ ExpenseTrackerComponents/ModifyButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ExpenseTrackerComponents
+{
+    public class ModifyButton:ExpenseTrackerButton
+    {
+        public ModifyButton()
+        {
+            this.Background = Brushes.SeaGreen;
+            this.BorderBrush = Brushes.SeaGreen;
+        }
+
+    }
+}
