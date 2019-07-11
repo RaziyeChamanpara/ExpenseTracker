33c8399381adc9b9ccdf64c19de6dc40d1469866
RaziyeChamanpara <r.chamanpara@gmail.com>
2019-07-11 15:15:54 +1000

    merge AddExpenseTypeWindow and EditExpenseTypeWindow

---
+++ ExpenseTracker/AddEditExpenseTypeWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess;
+using System.Windows;
+
+namespace ExpenseTracker
+{
+    public partial class AddEditExpenseTypeWindow : Window
+    {
+        public ExpenseType Model { get; set; } = new ExpenseType();
+
+        public AddEditExpenseTypeWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool? ShowForAdd()
+        {
+            this.Title = "Add Expense Type";
+            return this.ShowDialog();
+        }
+
+        public bool? ShowForEdit(ExpenseType expenseType)
+        {
+            Model = expenseType;
+            this.Title = "Edit Expense Type";
+            nameTextBox.Text = expenseType.Name;
+            return this.ShowDialog();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = nameTextBox.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Choose a name");
+                return;
+            }
+
+            Model.Name = name;
+
+            this.DialogResult = true;
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+    }
+}
