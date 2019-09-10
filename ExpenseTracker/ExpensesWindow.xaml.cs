2d349faa83e4a0fd228f8b1c56b513f3643bb789
RaziyeChamanpara <r.chamanpara@gmail.com>
2019-09-10 14:39:19 +1000

    inherit from BaseListWindow instead of Window

--- ExpenseTracker/ExpensesWindow.xaml.cs
+++ ExpenseTracker/ExpensesWindow.xaml.cs
@@ -1,84 +1,38 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using DataAccess;
 
 namespace ExpenseTracker
 {
-    public partial class ExpensesWindow : Window
+    public partial class ExpensesWindow : BaseListWindow<Expense>
     {
-        ExpenseRepository ExpenseRepository { get; set; } = new ExpenseRepository();
-        private List<Expense> _expenses = new List<Expense>();
-        private int _selectedIndex = -1;
-
-        public ExpensesWindow()
+        public ExpensesWindow() :
+            base(new ExpenseRepository(), new AddEditExpenseWindow())
         {
             InitializeComponent();
+            Initialize(expensesDataGrid);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _expenses = ExpenseRepository.GetAll();
-            expensesDataGrid.ItemsSource = _expenses;
-            expensesDataGrid.SelectedItem = _expenses.First();
-            _selectedIndex = 0;
-
+            expensesDataGrid.ItemsSource = List;
+            expensesDataGrid.SelectedItem = List.First();
+            selectedIndex = 0;
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            var addWindow= new AddEditExpenseWindow();
-            bool? result = addWindow.ShowForAdd();
-
-            if (result == false)
-                return;
-
-            Expense newExpense = addWindow.Model;
-            ExpenseRepository.Add(newExpense);
-            _expenses.Add(newExpense);
-            expensesDataGrid.Items.Refresh();
+            AddClick();
         }
 
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
-            _selectedIndex = expensesDataGrid.SelectedIndex;
-
-            if (_selectedIndex == -1)
-            {
-                MessageBox.Show("Please choose a row for edit.");
-                return;
-            }
-
-            var _selectedRecord = _expenses[_selectedIndex];
-
-            AddEditExpenseWindow editWindow = new AddEditExpenseWindow();
-
-            var result = editWindow.ShowForEdit(_selectedRecord);
-
-            if (result == false)
-                return;
-
-            ExpenseRepository.Update(_selectedRecord);
-            _expenses[_selectedIndex] = editWindow.Model;
-            expensesDataGrid.Items.Refresh();
+            EditClick();
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            if (_expenses.Count() == 0)
-                return;
-
-            Expense selectedRow = _expenses[_selectedIndex];
-
-            MessageBoxResult messageBoxResult = MessageBox
-                .Show(selectedRow.Description + " will be removed." , "Delete", MessageBoxButton.OKCancel);
-
-            if (messageBoxResult == MessageBoxResult.Cancel)
-                return;
-
-            ExpenseRepository.Remove(selectedRow.Id);
-            _expenses.Remove(selectedRow);
-            expensesDataGrid.Items.Refresh();
+            DeleteClick();
         }
 
     }
