22ef02429f219660404cd5499ecabdbaf48f8e2f
RaziyeChamanpara <r.chamanpara@gmail.com>
2019-09-10 14:40:25 +1000

    Create BaseModifyWindow

---
+++ ExpenseTracker/BaseModifyWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using DataAccess;
+
+namespace ExpenseTracker
+{
+    public abstract class BaseModifyWindow<T>:Window
+    {
+        public T Model;
+        public BaseModifyWindow(T model)
+        {
+            Model = model;
+        }
+        public abstract bool? ShowForAdd();
+
+        public abstract bool? ShowForEdit(T oldRecord);
+    }
+}
