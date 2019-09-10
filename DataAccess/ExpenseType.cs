d38809a0307ecb0873dc1a68492f512b6f96920e
RaziyeChamanpara <r.chamanpara@gmail.com>
2019-09-10 14:41:56 +1000

    inherit from IEntity interface

--- DataAccess/ExpenseType.cs
+++ DataAccess/ExpenseType.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DataAccess
 {
-    public class ExpenseType
+    public class ExpenseType:IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
